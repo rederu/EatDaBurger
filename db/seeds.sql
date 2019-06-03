608388ca5ad91324c3df6e5a2a7fc3893391039c
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-06-03 00:30:04 -0500

    Added initial files.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Black Pearl", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whooper", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avocado Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mystery Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Burger", true);
