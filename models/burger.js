18f45e84a346ad72f8c477ea004c264eeae20a47
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-06-07 02:22:54 -0500

    Added model setup to call the ORM functions

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,25 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
+
+var burger ={
+    displayAll: function(cb){
+        orm.displayAll("burgers", function(res){
+            cb(res);
+        });
+    }, //end displayAll
+
+    insertOne: function (cols, vals, cb) {
+		orm.inserOne("burgers", cols, vals, function (res) {
+			cb(res);
+		});
+	},
+
+    updateOne: function (objColVals, condition, cb) {
+		orm.insertOne("burgers", objColVals, condition, function (res) {
+			cb(res);
+		});
+	},
+};//End burger
+
+//Export module burger
+module.exports = burger;
